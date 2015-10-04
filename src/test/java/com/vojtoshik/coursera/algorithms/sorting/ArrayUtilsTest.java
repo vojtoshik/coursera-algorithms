76177c7f3ed3ec4a33c19d847233c3f08440062d
Anton Voitovych <vojtoshik@gmail.com>
2015-10-04 19:34:28 +0200

    Added test for ArrayUtils.shuffle

--- src/test/java/com/vojtoshik/coursera/algorithms/sorting/ArrayUtilsTest.java
+++ src/test/java/com/vojtoshik/coursera/algorithms/sorting/ArrayUtilsTest.java
@@ -1,9 +1,12 @@
 package com.vojtoshik.coursera.algorithms.sorting;
 
-
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotEquals;
 
 public class ArrayUtilsTest {
 
@@ -14,5 +17,30 @@ public class ArrayUtilsTest {
         ArrayUtils.swap(testArray, 0, 1);
         assertEquals(Integer.valueOf(3), testArray[0]);
         assertEquals(Integer.valueOf(2), testArray[1]);
+    }
+
+    @Test
+    public void testShuffle() {
+
+        int numberOfElements = 100;
+        Random random = new Random();
+
+        Integer[] original = new Integer[numberOfElements];
+
+        for (int i = 0; i < numberOfElements; i++) {
+            original[i] = random.nextInt();
+        }
+
+        Integer[] arrayToShuffle = Arrays.<Integer>copyOf(original, numberOfElements);
+
+        ArrayUtils.shuffle(arrayToShuffle);
+
+        assertEquals(numberOfElements, arrayToShuffle.length);
+        assertNotEquals(original, arrayToShuffle);
+
+        Arrays.sort(original);
+        Arrays.sort(arrayToShuffle);
+
+        assertEquals(original, arrayToShuffle);
     }
 }
