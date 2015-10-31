ad0d10dfb0cb18b608bb9a5bd524daef655867b6
Anton Voitovych <vojtoshik@gmail.com>
2015-10-31 15:26:14 +0100

    Provided more description for the 8puzzle problem

--- src/main/assignments/8puzzle/Solver.java
+++ src/main/assignments/8puzzle/Solver.java
@@ -1,11 +1,26 @@
 /**
  * Write a program to solve the 8-puzzle problem (and its natural generalizations) using the A* search algorithm.
  *
+ * Your program should work correctly for arbitrary N-by-N boards (for any 2 ≤ N < 128), even if it is too slow to solve
+ * some of them in a reasonable amount of time.
+ *
+ * You may not call any library functions other those in java.lang, java.util, and algs4.jar. You must use MinPQ for the
+ * priority queue(s).
+ *
+ * <i>Performance requirements</i>
+ * Your implementation should support all Board methods in time proportional to N2 (or better) in the worst case.
+ *
+ * <i>Corner cases</i>
+ * The constructor should throw a {@link java.lang.NullPointerException} if passed a null argument.
+ *
  * @author Anton Voitovych <vojtoshik@gmail.com>
  */
 public class Solver {
-    public Solver(Board initial) {
 
+    public Solver(Board initial) {
+        if (initial == null) {
+            throw new NullPointerException("Initial board can't be null");
+        }
     }
 
     public boolean isSolvable() {
