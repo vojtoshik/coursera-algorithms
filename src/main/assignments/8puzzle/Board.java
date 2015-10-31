5ef8636387e09140d2dee6a3fb97e36592ebf45a
Anton Voitovych <vojtoshik@gmail.com>
2015-10-31 15:21:12 +0100

    Added classes with API for 4th week's programming assignment

---
+++ src/main/assignments/8puzzle/Board.java
@@ -0,0 +1,40 @@
+/**
+ * @author Anton Voitovych <vojtoshik@gmail.com>
+ */
+public class Board {
+    public Board(int[][] blocks) {
+
+    }
+
+    public int dimension() {
+        return 0;
+    }
+
+    public int hamming() {
+        return 0;
+    }
+
+    public boolean isGoal() {
+        return false;
+    }
+
+    public int manhattan() {
+        return 0;
+    }
+
+    public Board twin() {
+        return null;
+    }
+
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/main/assignments/8puzzle/Solver.java
@@ -0,0 +1,22 @@
+/**
+ * Write a program to solve the 8-puzzle problem (and its natural generalizations) using the A* search algorithm.
+ *
+ * @author Anton Voitovych <vojtoshik@gmail.com>
+ */
+public class Solver {
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+        return false;
+    }
+
+    public int moves() {
+        return 0;
+    }
+
+    public Iterable<Board> solution() {
+        return null;
+    }
+}
